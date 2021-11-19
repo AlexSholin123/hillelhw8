7ba05bd347dd6aa89a33727b7b6465c36bf58f90
Alexandr Sholin <alex.sholin@icloud.com>
2021-11-19 19:15:54 +0200

    add Animal.java AnimalFactory.java Cat.java Dog.java Main.java

---
+++ homework17/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return "Я кот. Зовут меня - " + super.getName();
+    }
+}
